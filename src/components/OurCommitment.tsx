417a7ff45e8543e2f9f446ae13316138aea54d97
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-08-15 03:42:45 -0300

    agregar componente de compromiso de la plataforma

---
+++ src/components/OurCommitment.tsx
@@ -0,0 +1,20 @@
+export const OurCommitment = () => {
+    return (
+        <section
+            aria-labelledby="our-commitment-title"
+            className="flex items-center justify-center bg-white-fa px-4 md:px-6 lg:px-24 xl:px-32 w-full py-28 md:py-32 xl:py-40"
+        >
+            <div className="max-w-3xl w-full text-black text-sm md:text-base xl:text-lg">
+                <h2 id="our-commitment-title" className="sr-only">
+                    Nuestro Compromiso
+                </h2>
+                <p className="font-normal">
+                    En Cranbelo, somos más que una plataforma educativa. Somos un equipo de apasionados por el aprendizaje, dedicados a crear experiencias educativas que inspiran y transforman. Nuestro compromiso es contigo, para ayudarte a alcanzar tus metas empresariales y profesionales.
+                </p>
+                <p className="mt-2 font-medium">
+                    Conoce al equipo detrás de todo esto.
+                </p>
+            </div>
+        </section>
+    );
+};
