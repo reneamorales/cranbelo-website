073c3cb9a1307f3a9b69849d4a80760fe535b15d
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-08-18 23:59:59 -0300

    agregar componente de metricas del sitio

---
+++ src/components/Report/Numbers.tsx
@@ -0,0 +1,17 @@
+
+const Numbers = () => {
+  return (
+    <div>
+        <label>+1200</label>
+        <p>Cursos completos en diversas áreas para emprendedores y profesionales en constante evolución.</p>
+
+        <label>+950</label>
+        <p>Estudiantes satisfechos es testimonio de la calidad y efectividad de nuestros programas.</p>
+
+        <label>+250</label>
+        <p>Empresas y profesionales activos, eligiendo nuestra plataforma para su desarrollo.</p>
+    </div>
+  )
+}
+
+export default Numbers
