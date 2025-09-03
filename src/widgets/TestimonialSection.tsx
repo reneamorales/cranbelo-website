45bb6f01d7ae4202b864cb2343997849ab4b2dec
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-09-03 00:16:30 -0300

    renombramiento de TestimonialsPlatforms.tsx a TestimonialSection.tsx

--- src/widgets/TestimonialSection.tsx
+++ src/widgets/TestimonialSection.tsx
@@ -1,9 +1,10 @@
 import { ChevronLeft, ChevronRight } from "../icons/Chevrons";
+import type { TestimonialPlatformCardProps } from "../types/TestimonialPlatformCardProps";
 import { TestimonialPlatformCard } from "../shared/components/TestimonialPlatformCard";
 import { useRef, useState, useEffect } from "react";
 import clsx from "clsx";
 
-export const TestimonialsPlatforms = () => {
+export const TestimonialSection = ({ testimonialsPlatform }) => {
   const scrollRef = useRef<HTMLDivElement>(null);
   const [atStart, setAtStart] = useState(true);
   const [atEnd, setAtEnd] = useState(false);
@@ -41,7 +42,7 @@ export const TestimonialsPlatforms = () => {
 
   return (
     <section className="relative bg-darkGray px-4 md:px-6 lg:px-24 xl:px-32 w-full py-9 md:py-12 xl:py-16 h-auto">
-      <h2 className="text-lg leading-7 md:text-2xl md:leading-9 xl:text-4xl xl:leading-14 font-bold text-center mb-6 text-black underline">
+      <h2 className="text-lg leading-7 md:text-2xl md:leading-9 xl:text-4xl xl:leading-14 font-bold text-center mb-6 text-purple-light underline">
         Lo que dicen nuestros alumnos sobre Cranbelo
       </h2>
 
@@ -52,8 +53,8 @@ export const TestimonialsPlatforms = () => {
         className={clsx(
           "absolute left-2 top-1/2 -translate-y-1/2 z-10 p-2 rounded-full shadow-md text-white transition-all duration-200 transform",
           atStart
-            ? "bg-cran-surface-700 cursor-not-allowed scale-100"
-            : "bg-cran-surface-400 hover:bg-cran-surface-300 hover:scale-105 active:bg-cran-surface-200 active:scale-95"
+            ? "bg-surface-cran-700 cursor-not-allowed scale-100"
+            : "bg-surface-cran-400 hover:bg-surface-cran-300 hover:scale-105 active:bg-surface-cran-200 active:scale-95"
         )}
       >
         <ChevronLeft className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10" />
@@ -62,12 +63,11 @@ export const TestimonialsPlatforms = () => {
       {/* Contenedor scrolleable */}
       <div
         ref={scrollRef}
-        className="flex overflow-x-auto gap-4 md:gap-6 xl:gap-8 scroll-smooth scrollbar-hide bg-deepGray"
+        className="flex overflow-x-auto gap-4 md:gap-6 xl:gap-8 py-2 md:py-3 xl:py-4 scroll-smooth scrollbar-hide bg-deepGray"
       >
-        <TestimonialPlatformCard />
-        <TestimonialPlatformCard />
-        <TestimonialPlatformCard />
-        <TestimonialPlatformCard />
+       {testimonialsPlatform.map((t, index) => (
+        <TestimonialPlatformCard key={index} {...t} />
+      ))}
       </div>
 
       {/* Botón derecha */}
@@ -77,8 +77,8 @@ export const TestimonialsPlatforms = () => {
         className={clsx(
           "absolute right-2 top-1/2 -translate-y-1/2 z-10 p-2 rounded-full shadow-md text-white transition-all duration-200 transform",
           atEnd
-            ? "bg-cran-surface-700 cursor-not-allowed scale-100"
-            : "bg-cran-surface-400 hover:bg-cran-surface-300 hover:scale-105 active:bg-cran-surface-200 active:scale-95"
+            ? "bg-surface-cran-700 cursor-not-allowed scale-100"
+            : "bg-surface-cran-400 hover:bg-surface-cran-300 hover:scale-105 active:bg-surface-cran-200 active:scale-95"
         )}
       >
         <ChevronRight className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10" />
