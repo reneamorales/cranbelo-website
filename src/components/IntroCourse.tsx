f0dcf8b093cbd53824a297ef8f77edc810e547a4
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-09-13 00:35:28 -0300

    refactor(IntroCourseSection/IntroCourse): mover label y title al section y renderizar bloques de secciones dinámicamente

--- src/components/IntroCourse.tsx
+++ src/components/IntroCourse.tsx
@@ -1,12 +1,10 @@
 import type { IntroCourseProps } from "../types/IntroCourseProps";
 
-const IntroCourse = ({ label, title, subtitle, body }: IntroCourseProps) => {
+const IntroCourse = ({subtitle, body }: IntroCourseProps) => {
   return (
     <div className="flex flex-col w-full">
-      <span className="font-bold text-base leading-lh-base md:text-xl md:leading-lh-2xl  xl:text-altxl xl:leading-lh-altxl text-surface-cran-white block mb-2">{label}</span>
-      <h2 className="text-brand-support-strong content-start-start mb-3 h2">{title}</h2>
-      <h3 className="font-medium text-sm leading-lh-sm md:text-base md:leading-lh-base xl:text-lg xl:leading-lh-xl text-info-cran-500 mb-4">{subtitle}</h3>
 
+      <h3 className="font-medium text-sm leading-lh-sm md:text-base md:leading-lh-base xl:text-lg xl:leading-lh-xl text-info-cran-500 mb-4">{subtitle}</h3>
       {Array.isArray(body) ? (
         <ul className="list-disc list-inside space-y-2">
           {body.map((item, i) => (
@@ -14,7 +12,7 @@ const IntroCourse = ({ label, title, subtitle, body }: IntroCourseProps) => {
           ))}
         </ul>
       ) : (
-        <p className="font-regular text-neutral-primary text-sm text-leading-lh-sm md:text-base md:leading-lh-base xl:text-lg xl:leading-lh-xl">{body}</p>
+        <p className="font-regular text-neutral-primary text-sm text-leading-lh-sm md:text-base md:leading-lh-base xl:text-lg xl:leading-lh-xl mb-3 md:mb-4 xl:mb-5">{body}</p>
       )}
     </div>
   );
--- src/widgets/IntroCourseSection.tsx
+++ src/widgets/IntroCourseSection.tsx
@@ -1,14 +1,16 @@
 import IntroCourse from "../components/IntroCourse";
 import type { IntroCourseProps } from "../types/IntroCourseProps";
 
-export const IntroCourseSection = ({ sections, title }: { sections: IntroCourseProps[] }) => {
+export const IntroCourseSection = ({ sections, title }: { sections: IntroCourseProps[]; title: string }) => {
   return (
     <section className="flex flex-col items-center bg-darkGray section">
-      <span className="font-bold text-base leading-lh-base md:text-xl md:leading-lh-2xl  xl:text-altxl xl:leading-lh-altxl text-surface-cran-white block mb-2">{label}</span>
-      <h2 className="text-brand-support-strong content-start-start mb-3 h2">{title}</h2>
-      {sections.map((block, index) => (
-        <IntroCourse key={index} {...block} />
-      ))}
+      <div className="container">
+        <span className="font-bold text-base leading-lh-base md:text-xl md:leading-lh-2xl  xl:text-altxl xl:leading-lh-altxl text-surface-cran-white block mb-2">Curso</span>
+        <h2 className="text-brand-support-strong content-start-start mb-3 h2">{title}</h2>
+        {sections.map((block, index) => (
+          <IntroCourse key={index} {...block} />
+        ))}
+      </div>
     </section>
   );
 };
