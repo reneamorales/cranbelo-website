9e1c536e7cdee354fca549f2a8da48412ba881b3
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-09-15 12:28:12 -0300

    feat: update CoursesLists component with new CourseList type and add course details link
    - Replaced 'Course' type with 'CourseList' in the 'CoursesLists' component.
    - Added a 'Link' component inside 'CourseCard' to navigate to detailed course view.
    - Updated the 'cta' prop in 'CourseCard' to include a styled 'Ver más detalles' button with a link to the course details page.

--- src/widgets/CourseSection/CoursesLists.tsx
+++ src/widgets/CourseSection/CoursesLists.tsx
@@ -1,8 +1,9 @@
-import type { Course } from "../../types/Course";
+import type { CourseList } from "../../types/CourseList";
 import { CourseCard } from "../../shared/components/CourseCard";
+import { Link } from "react-router-dom";
 
 type Props = {
-  courses: Course[];
+  courses: CourseList[];
 };
 
 export const CoursesLists = ({ courses }: Props) => {
@@ -21,7 +22,15 @@ export const CoursesLists = ({ courses }: Props) => {
 
         <div className="grid grid-cols-2 xl:grid-cols-3 gap-4 md:gap-6 xl:gap-8">
           {courses.map((course) => (
-            <CourseCard key={course.id} {...course} />
+            <CourseCard key={course.id} {...course}
+              cta={
+                <Link
+                  to={`/course/${course.id}`}
+                  className="mt-auto text-center font-semibold text-sm md:text-lg xl:text-[26px] sm:px-4 px-5 py-1 md:px-20 md:py-4 xl:px-12 xl:py-2.5 bg-brand-secondary-strong/15 hover:bg-brand-secondary-strong/20 text-neutral-primary hover:text-brand-secondary-strong border-[1.6px] md:border-[1.8px] xl:border-2 border-transparent hover:border-brand-secondary-strong active:border-transparent active:bg-neutral-secondary active:text-secondary-cran-900 w-full inline-block rounded-md transition-all duration-300 ease-in-out"
+                >
+                  Ver más detalles
+                </Link>
+              } />
           ))}
         </div>
       </div>
