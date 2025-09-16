0a1bd4eb35b06320c0e65180bd9d815a46a8d438
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-09-15 23:59:33 -0300

    fix: ajustar tamaño de íconos  y
    - Se redujo el tamaño de los íconos de  a  para mejorar la consistencia visual.
    - Modificación en las clases CSS de los íconos en el archivo .

--- src/icons/Chevrons.tsx
+++ src/icons/Chevrons.tsx
@@ -29,13 +29,13 @@ export const ChevronLeft = ({ className }: IconProps) => (
 );
 
 export const ChevronDownMini = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-4">
     <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
   </svg>
 );
 
 export const ChevronUpMini = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-4">
   <path fill-rule="evenodd" d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z" clip-rule="evenodd" />
 </svg>
 
