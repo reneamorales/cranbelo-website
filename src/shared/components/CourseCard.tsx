2a3153545135ba75d47acc772f21bc1e8bc7c631
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-09-18 23:58:43 -0300

    Refactor: update CourseCard to accept cta prop and use Link component

--- src/shared/components/CourseCard.tsx
+++ src/shared/components/CourseCard.tsx
@@ -1,6 +1,7 @@
-import type { Course } from "../../types/Course";
+import { Link } from "react-router-dom";
+import type { CourseList } from "../../types/CourseList";
 
-export const CourseCard = ({ id, title, description, image }: Course) => (
+export const CourseCard = ({ id, title, description, image, cta}: CourseList) => (
 
   <article id={id.toString()} className="bg-inverse-neutral-primary rounded-lg md:rounded-xl xl:rounder-2xl shadow-md">
     <img src={image} alt="" className="rounded-t-lg md:rounded-t-xl xl:rounded-t-2xl w-full h-auto" />
@@ -13,12 +14,7 @@ export const CourseCard = ({ id, title, description, image }: Course) => (
       <p className="font-medium text-neutral-secondary text-xs md:text-base xl:text-2xl min-h-[12em]">
         {description}
       </p>
-      <a
-        href="#"
-        className="mt-auto text-center font-semibold text-sm md:text-lg xl:text-[26px] sm:px-4 px-5 py-1 md:px-20 md:py-4 xl:px-12 xl:py-2.5 bg-o bg-brand-secondary-strong/15 hover:bg-brand-secondary-strong/20   text-neutral-primary hover:text-brand-secondary-strong border-[1.6px] md:border-[1.8px] xl:border-2 border-transparent hover:border-brand-secondary-strong active:border-transparent active:bg-neutral-secondary active:text-secondary-cran-900 w-full inline-block rounded-md trasnsition-all duration-300 ease-in-out"
-      >
-        Ver más detalles
-      </a>
+      {cta && <div className="mt-4">{cta}</div>}
     </div>
   </article>
 );
