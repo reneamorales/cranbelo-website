b1b1453c0769a1d338519d6e176dafffee1512d4
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-10-08 17:15:39 -0300

    agregar componente de página PaymentMethodsPage.tsx

---
+++ src/pages/PaymentMethodsPage.tsx
@@ -0,0 +1,9 @@
+import {PaymentMethods} from "../components/PaymentMethods"
+
+const PaymentMethodsPage = () => {
+    return (
+        <PaymentMethods />
+    )
+}
+
+export default PaymentMethodsPage
