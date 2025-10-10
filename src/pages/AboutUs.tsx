a487977f247c7f1ea373c3dbaf1cd126ae6f1332
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-10-09 23:58:49 -0300

    agregar nuevo componente de página AboutUs.tsx

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,20 @@
+import { HeroSection } from "../components/HeroTeam"
+import { IntroTeamsSection } from "../components/IntroTeam"
+import { VideoPresentation } from "../components/VideoPresentation"
+import { MissionAndValues } from "../components/MissionAndValues"
+import { TeamProfiles } from "../components/TeamProfiles"
+import { Impact } from "../components/Impact" 
+const AboutUs = () => {
+    return (
+        <>
+            <HeroSection />
+            <IntroTeamsSection />
+            <VideoPresentation />
+            <MissionAndValues />
+            < TeamProfiles />
+            < Impact />
+        </>
+    )
+}
+
+export default AboutUs 
