ff1f497be25f0723fccc41639ccccbc852d42417
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-10-10 23:50:29 -0300

    agregar nuevos componenete de icono de Plus y Close

---
+++ src/icons/CloseX.tsx
@@ -0,0 +1,13 @@
+type IconProps = {
+    className?: string;
+};
+const CloseX =  ({ className }: IconProps) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={className}>
+            <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+
+    )
+}
+
+export default CloseX
