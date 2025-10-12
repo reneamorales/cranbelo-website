1ab0220d48022bd8dace9f0d86c65fab49a563f7
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-10-12 00:26:35 -0300

    agregar tipado de PricingCourse

---
+++ src/data/PricingCourse.ts
@@ -0,0 +1,130 @@
+import type { PricingDataProps } from "../types/PricingDataProps";
+
+export const pricingData: PricingDataProps[] = [
+  {
+    id: 1,
+    title: "Marketing Digital Avanzado",
+    pricing: [
+      {
+        type: "Pago Único",
+        price: "80.000",
+      },
+      {
+        type: "Suscripción Mensual",
+        price: "10.000",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Ciencia de Datos",
+    pricing: [
+      {
+        type: "Pago Único",
+        price: "95.000",
+      },
+      {
+        type: "Suscripción Mensual",
+        price: "12.000 ",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Diseño UX/UI",
+    pricing: [
+      {
+        type: "Pago Único",
+        price: "85.000",
+      },
+      {
+        type: "Suscripción Mensual",
+        price: "11.000 ",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Programación Backend",
+    pricing: [
+      {
+        type: "Pago Único",
+        price: "90.000",
+      },
+      {
+        type: "Suscripción Mensual",
+        price: "11.500 ",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Programación Frontend",
+    pricing: [
+      {
+        type: "Pago Único",
+        price: "85.000",
+      },
+      {
+        type: "Suscripción Mensual",
+        price: "$11.000 ",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Chatbot 101",
+    pricing: [
+      {
+        type: "Pago Único",
+        price: "$75.000",
+      },
+      {
+        type: "Suscripción Mensual",
+        price: "$9.500 ",
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: "Estrategías de SEO",
+    pricing: [
+      {
+        type: "Pago Único",
+        price: "$80.000",
+      },
+      {
+        type: "Suscripción Mensual",
+        price: "$10.000 ",
+      },
+    ],
+  },
+  {
+    id: 8,
+    title: "Gestión de Proyectos Ágiles",
+    pricing: [
+      {
+        type: "Pago Único",
+        price: "$82.000",
+      },
+      {
+        type: "Suscripción Mensual",
+        price: "$10.500 ",
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: "Fundamentos de Machine Learning",
+    pricing: [
+      {
+        type: "Pago Único",
+        price: "$98.000",
+      },
+      {
+        type: "Suscripción Mensual",
+        price: "$12.500 ",
+      },
+    ],
+  },
+];
