cc1411a7cda10534b78eb1deff66bbd556a3cf7c
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-10-13 18:25:10 -0300

    style: agregar nueva clase se utilidad h1 para la mejora de la semática e implmentación dentro de una de las secciones

--- src/widgets/IntroCourseSection.tsx
+++ src/widgets/IntroCourseSection.tsx
@@ -6,7 +6,7 @@ export const IntroCourseSection = ({ sections, title }: { sections: IntroCourseP
     <section className="flex flex-col items-center bg-darkGray section">
       <div className="container">
         <span className="font-bold text-base leading-lh-base md:text-xl md:leading-lh-2xl  xl:text-altxl xl:leading-lh-altxl text-surface-cran-white block mb-2">Curso</span>
-        <h2 className="text-brand-support-strong content-start-start mb-3 h2">{title}</h2>
+        <h1 className="text-brand-support-strong content-start-start mb-3 h1">{title}</h1>
         {sections.map((block, index) => (
           <IntroCourse key={index} {...block} />
         ))}
