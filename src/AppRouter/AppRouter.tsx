4b6b57daaa3c35f3955bf626d5f9468a97298e1f
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-10-17 21:16:47 -0300

    feat(router): agrega nuevas rutas y mejora la estructura de navegación - Se actualiza el AppRouter para incluir nuevas rutas: - /preguntas-frecuentes → FaqsPage - /formas-de-pagos → PaymentMethodsPage - /tyc → TeryConPage - /#contacto → redirección a la sección de contacto - manejo de ruta 404 con Page404 - Además, se reemplazan y renombran rutas antiguas para mantener una navegación más coherente y semántica en la aplicación.

--- src/AppRouter/AppRouter.tsx
+++ src/AppRouter/AppRouter.tsx
@@ -3,7 +3,11 @@ import ScrollToTop from "../ScrollToTop/ScrollToTop";
 import Home from "../pages/Home";
 import AboutUs from "../pages/AboutUs";
 import CoursePage from "../pages/CoursePage";
+import FaqsPage from "../pages/FaqsPage";
 import PaymentMethodsPage from "../pages/PaymentMethodsPage";
+import TeryConPage from "../pages/TeryConPage";
+import Page404 from "../pages/Page404";
+
 import { Header } from "../widgets/Header";
 import { Footer } from "../widgets/Footer";
 
@@ -15,13 +19,13 @@ export const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<Navigate to="/home" replace />} />
                 <Route path="/home" element={<Home />} />
-
-                {/*<Route path="/course" element={<CoursePage />} />*/}
                 <Route path="/course/:id" element={<CoursePage />} />
-                <Route path="/about" element={<AboutUs />} />
-                <Route path="/payments" element={<PaymentMethodsPage />} />
-                <Route path="*" element={<Navigate to="/404" />} />
-
+                <Route path="/nosotros" element={<AboutUs />} />
+                <Route path="/preguntas-frecuentes" element={<FaqsPage />} />
+                <Route path="/#contacto" element={<Navigate to="/contacto" replace/>} />
+                <Route path="/formas-de-pagos" element={<PaymentMethodsPage />} />
+                <Route path="/tyc" element={<TeryConPage />} />
+                <Route path="*" element={<Page404 />} />
             </Routes>
             <Footer />
         </BrowserRouter>
